2070765b7233d321130955307ee9c9b927a56dd9
dilmanpreet <dilmanpreet1@gmail.com>
2018-05-31 15:21:24 -0700

    Recipe class changed
    added info for the ingredient class.
    1. RecipeTime Added
    2. Add Item Function changed to Add ingredient
    Changes to add ingredient

--- src/app/ingredient.class.ts
+++ src/app/ingredient.class.ts
@@ -1,15 +1,21 @@
 //making and exporting the ingredient class
 export class ingredient{
     
+
+
+    /**
+     * @param name Name Of Ingredient
+     * @param quantity Quantity Of Ingredient
+     */  
     constructor(public name : string, public quantity : number){
         
     }
 
-  // adds and increses the quantity by one
+  // adds and increses the quantity by the number
     add(temp: number){
     this.quantity+= temp ;
     }
-    //decreases the quantity by one
+    //decreases the quantity by the number
     subtract(temp: number){
     this.quantity-=temp;
     
--- src/app/recipe.class.ts
+++ src/app/recipe.class.ts
@@ -1,20 +1,38 @@
 import {ingredient} from "./ingredient.class";
 
 export class recipe{
-    ingredients : Array<ingredient> =[];
-    instructions : Array<String> = [];
-
+    ingredientsList : Array<ingredient> =[];
+    instructionsList : Array<String> = [];
+    recipeTime:number;
     
-    addItem(name: string, quantity: number){
-        
-        this.ingredients.push(new ingredient(name, quantity));
-    }
+      /**
+     * @function addItem Adds the Item to the IngredientList
+     * @param Ingredient It is the Ingredient being added to the Recipe 
+     */
+    addIngredient(nowingredient:ingredient){
+        if(this.ingredientsList.length==0){
+            this.ingredientsList.push(nowingredient);
+        }
+        else{
+            for(let count of this.ingredientsList){
+                if(count.name==nowingredient.name){
+                    count.quantity+=nowingredient.quantity;
+                    return;
+                this.ingredientsList.push(nowingredient);
+            }
+        }
+     }
 
+      /**
+     * @function addInstruction Add Instruction for the Recipe
+     * @param instruction Instruction  Being Added
+     */
     addInstruction(inst: String){
-       this.instructions.push(inst);
+       this.instructionsList.push(inst);
     }
 
-     constructor(public time: string){        
+     constructor(public time: number){ 
+         time=this.recipeTime;    
      }
  
 }
