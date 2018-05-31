70482f3524ca9afe6b36c62cddb3dff2f4767556
dilmanpreet <dilmanpreet1@gmail.com>
2018-05-31 16:27:46 -0700

    adding the fomsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';  // <-- NgModel lives here
 import { AppComponent } from './app.component';
+import { ReceipeCompComponent } from './receipe-comp/receipe-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReceipeCompComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
