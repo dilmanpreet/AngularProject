2d6a7930f3094a1d0b31c80738fec2e2d6466cf9
dilmanpreet <dilmanpreet1@gmail.com>
2018-06-01 01:24:07 -0700

    making test work for recipe gui
    adding forms module and app component to the specs file

--- src/app/recipe-comp/recipe-comp.component.spec.ts
+++ src/app/recipe-comp/recipe-comp.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/Forms';
+import { AppComponent } from '../app.component';
 import { recipeCompComponent } from './recipe-comp.component';
 
 describe('recipeCompComponent', () => {
@@ -8,7 +9,7 @@ describe('recipeCompComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ recipeCompComponent ]
+      declarations: [ recipeCompComponent, AppComponent ]
     })
     .compileComponents();
   }));
