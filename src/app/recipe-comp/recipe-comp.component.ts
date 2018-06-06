f82659d8134b4b884e721332fef53c22b63b291b
dilmanpreet <dilmanpreet1@gmail.com>
2018-06-06 14:50:00 -0700

    delete button working
    added new hide menu button

--- src/app/recipe-comp/recipe-comp.component.ts
+++ src/app/recipe-comp/recipe-comp.component.ts
@@ -10,74 +10,50 @@ import {recipe} from "../recipeapp/recipe.class";
 export class RecipeCompComponent implements OnInit {
   
   
-  recipeName : string = "";
-  newTime:number =0;
-  
-  // time : Array<string> = ["30 min","40 min","10 min"];
-
-  recipe : Array<recipe> = [new recipe(" ", 0)
-  // new recipe("mango shake", 5),
-  // new recipe("banana shake", 3),
-  // new recipe("french fries", 12)
+  recipeArray : Array<recipe> = [new recipe(" ", 0),
+  new recipe("mango shake", 5),
+  new recipe("banana shake", 3),
+  new recipe("french fries", 12)
 ];
-
-  // ingredient : Array<ingredient> = [new ingredient("milk", 5),
-  // new ingredient("apple", 5),
-  // new ingredient("juice", 3),
-  // new ingredient("sugar", 12)];
-
-  // formBinding: ingredient = new ingredient("",0);
-  formBinding2: recipe = new recipe("",0);
-
-  selectedItem : recipe = null;
-  editName : recipe = null;
-  deleteItem : recipe = null;
-  // fastRecipe : Array<ingredient> = [new ingredient("milk", 5),
-  // new ingredient("apple", 5),
-  // new ingredient("juice", 3),
-  // new ingredient("sugar", 12)];
-   
+formBinding: recipe = new recipe("",0);
+formBinding2: recipe = new recipe("",0);
+selectedItem : recipe = null;
 
 
-  constructor() { }
+constructor() { }
 
-  ngOnInit() {
+ngOnInit() {
+
+}
+addRecipe() {
+  this.recipeArray.push(new recipe(this.formBinding.name,this.formBinding.time)); 
+
+}
+// selecting an item
+selectRecipe(sel: recipe){
+  this.selectedItem = sel;
+  this.formBinding2= sel;    
+}
+  // editing a recipe
+editRecipe(){
+this.selectedItem.name=this.formBinding2.name;
+this.selectedItem.time=this.formBinding2.time;
  
-  }
-  addRecipe() {
-    this.recipe.push(new recipe(this.formBinding2.name,this.formBinding2.time)); 
-
-  }
+}
+// deleting a recipe
+deleteRecipe(){
+  let index=this.recipeArray.indexOf(this.selectedItem);
+  this.recipeArray.splice(index,1);
   
-  //  addIngredient(){
+}
+hideMenu(){
+this.selectedItem = null;
+}
 
-  //   this.ingredient.push(new ingredient(this.formBinding.name,this.formBinding.quantity)); 
-  // }
+//  addIngredient(){
 
-  // selecting an item
-  select(recipe){
-
-    this.selectedItem = recipe;    
-  }
-    // editing a recipe
-  editRecipe(){
-  this.selectedItem.name=this.formBinding2.name;
- 
-  this.selectedItem.time=this.formBinding2.time;
-   
-  }
-  // deleting a recipe
-  deleteRecipe(){
-    let index=-1;
-    for(let i=0;i<=recipe.length;i++){
-      if(recipe.name==this.selectedItem.name){
-        index=i;
-      }
-      this.recipe.splice(index,1);
-  
-  }
-  }
-  
+//   this.ingredient.push(new ingredient(this.formBinding.name,this.formBinding.quantity)); 
+// }
 
 }
 
