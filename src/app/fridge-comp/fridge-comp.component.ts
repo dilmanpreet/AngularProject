e5914b5f77abe7b015ade6676b3a1bab0a5e8f1c
dilmanpreet <dilmanpreet1@gmail.com>
2018-06-07 01:23:05 -0700

    fridge components

--- src/app/fridge-comp/fridge-comp.component.ts
+++ src/app/fridge-comp/fridge-comp.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RmsService } from '../rms/rms.service';
+import {ingredient} from "../ingredient.class";
+import {recipe} from "../recipe-comp/recipe.class";
 
 @Component({
   selector: 'app-fridge-comp',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FridgeCompComponent implements OnInit {
 
-  constructor() { }
+  public service = new RmsService();
 
-  ngOnInit() {
-  }
+  constructor(service:RmsService) {   this.service = service; }
+
+  public  selectedItem: ingredient = null;
+  public  changeItem : ingredient =  new ingredient("",0);;
+  public  newItem : ingredient =  new ingredient("",0);;
+  
+
+
+ngOnInit() {
+
+}
+//adding Items to fridge
+addItemToFridge() {
+  this.service.ourFridge.addToFridge(new ingredient(this.newItem.name,this.newItem.quantity)); 
+
+}
+// selecting an ingredient
+selectIngredient(sel: ingredient){
+  this.selectedItem = sel;
+   
+}
+  // editing a quantity
+editQuantity(){
+  this.service.ourFridge.addToFridge(this.changeItem);
+ 
+}
+// removing a Ingredient
+removingIngredient(){
+  this.service.ourFridge.RemoveFromFridge(this.selectedItem,this.selectedItem.quantity);
+
+  
+}
+hideMenu(){
+this.selectedItem = null;
+}
+
+ 
 
 }
